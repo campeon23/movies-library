a230920ac25ab3c224887beff7db6081ded5df00
Marco Villarruel <mvilla@Ruriks-OpenSuse.local>
2023-01-11 13:21:40 -0800

    Initial Commit
    - First commit.

---
+++ movies_library/__init__.py
@@ -0,0 +1,17 @@
+import os, datetime
+from flask import Flask
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+from movies_library.routes import pages
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(pages)
+    app.config["DATABASE_URL"] = os.environ.get("DATABASE_URL")
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "BDaZquvb75pa88-oNBW1FCSReYSzAbBfcjLX-coAjPI")
+    app.db = MongoClient(app.config["DATABASE_URL"]).movies_library
+    
+    return app
